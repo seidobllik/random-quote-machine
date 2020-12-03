4d11055c7690d2ba00a1ad6f52936e7c5a0b2b0a
Tom <45657686+seidobllik@users.noreply.github.com>
2020-12-02 19:40:01 -0800

    updated json url https://github.com/skolakoda/programming-quotes-api/issues/37
    Issue above documents an error with the API service. Workaround is in place.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ class Application extends React.Component {
     super();
     this.state = {
       text: "",
-      author: "" 
+      author: ""
     };
     this.handleChange = this.handleChange.bind(this);
   }
@@ -17,24 +17,61 @@ class Application extends React.Component {
     let quote = await getQuote();
     this.setState({
       text: quote["text"],
-      author: quote["author"] }
-    );
+      author: quote["author"]
+    });
   }
 
   render() {
-    return (
-      React.createElement("div", { className: "jumbotron rounded-pill shadow w-50 mx-auto my-auto px-5 py-4", id: "quote-box" },
-      React.createElement("p", { className: "blockquote text-primary m-2", id: "text" }, React.createElement("i", { class: "fas fa-quote-left" }), " ", this.state.text, " ", React.createElement("i", { class: "fas fa-quote-right" })),
-      React.createElement("p", { className: "blockquote-footer text-secondary text-right", id: "author" }, this.state.author),
-      React.createElement("div", { className: "text-center" },
-      React.createElement("button", { type: "button", className: "btn btn-primary btn-sm mx-5", id: "new-quote", onClick: this.handleChange }, React.createElement("i", { class: "fas fa-redo" }), " Refresh"))));
+    return React.createElement(
+      "div",
+      {
+        className:
+          "jumbotron rounded-pill shadow w-50 mx-auto my-auto px-5 py-4",
+        id: "quote-box"
+      },
+      React.createElement(
+        "p",
+        { className: "blockquote text-primary m-2", id: "text" },
+        React.createElement("i", { class: "fas fa-quote-left" }),
+        " ",
+        this.state.text,
+        " ",
+        React.createElement("i", { class: "fas fa-quote-right" })
+      ),
+      React.createElement(
+        "p",
+        {
+          className: "blockquote-footer text-secondary text-right",
+          id: "author"
+        },
+        this.state.author
+      ),
+      React.createElement(
+        "div",
+        { className: "text-center" },
+        React.createElement(
+          "button",
+          {
+            type: "button",
+            className: "btn btn-primary btn-sm mx-5",
+            id: "new-quote",
+            onClick: this.handleChange
+          },
+          React.createElement("i", { class: "fas fa-redo" }),
+          " Refresh"
+        )
+      )
+    );
   }
 }
 
 async function getQuote() {
-  const url = "https://programming-quotes-api.herokuapp.com/quotes/random/lang/en";
+  const url =
+    "https://raw.githubusercontent.com/skolakoda/programming-quotes-api/master/backup/quotes.json";
   let response = await fetch(url);
   let json = await response.json();
+
+  json = json[Math.floor(Math.random() * (json.length + 1))];
 
   return {
     text: json["en"],
@@ -42,4 +79,7 @@ async function getQuote() {
   };
 }
 
-ReactDOM.render(React.createElement(Application, null), document.getElementById("app"));
+ReactDOM.render(
+  React.createElement(Application, null),
+  document.getElementById("app")
+);
